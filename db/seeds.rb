f8a59e6118a35e36be9960ee7560174f7d920433
tung nguyen <tungnt@arena.edu.vn>
2020-07-28 15:37:52 +0200

    Rails Wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+  Article.create(title: Faker::Hacker.say_something_smart, content: Faker::Quotes::Shakespeare.hamlet_quote)
+end
